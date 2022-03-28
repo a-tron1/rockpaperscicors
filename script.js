05741c029cccfb5f6199a3f616d9ffb15bf725c3
Aaron Neal <neal.aaron.m@gmail.com>
2022-03-28 17:08:05 -0400

    creat computerPlay function, start 1 round play

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-<script>
- 
+ /*console.log("hello world");*/
 
- 
-</script>
+
+/*const rock = 1;
+const paper = 2;
+const scissors = 3; */
+
+
+function computerPlay() {
+    let result = Math.floor(Math.random() * 3) + 1; 
+        if (result === 1) {
+            return "rock";
+        } else if (result === 2) {
+            return "paper";
+        } else if (result === 3) {
+            return "scissors";
+        }
+    }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection = "rock" && computerSelection = "scissors" )
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound());
