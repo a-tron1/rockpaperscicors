d8f3d58975d48170cec4c116063302e3a4d2e0b7
Aaron Neal <neal.aaron.m@gmail.com>
2022-03-29 14:49:07 -0400

    create playround function with win/lose/tie result

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
- /*console.log("hello world");*/
+console.log("hello world");
 
 
 /*const rock = 1;
 const paper = 2;
 const scissors = 3; */
 
-
+/* this block of code generates a random selection for the computer player*/
 function computerPlay() {
     let result = Math.floor(Math.random() * 3) + 1; 
         if (result === 1) {
@@ -17,11 +17,16 @@ function computerPlay() {
         }
     }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection = "rock" && computerSelection = "scissors" )
+function playRound(playerSelection , computerSelection) {
+    if ((playerSelection == "rock" && computerSelection == "scissors" ) || (playerSelection == "scissors" && computerSelection == "paper" ) || (playerSelection == "paper" && computerSelection == "rock" )) {
+        return("Human wins round");
+    }   else if  ((playerSelection == "rock" && computerSelection == "paper" ) || (playerSelection == "scissors" && computerSelection == "rock" ) || (playerSelection == "paper" && computerSelection == "scissors" )) {
+        return("Computer wins round");
+    } else if ((playerSelection == "rock" && computerSelection == "rock" ) || (playerSelection == "scissors" && computerSelection == "scissors" ) || (playerSelection == "paper" && computerSelection == "paper" )) {
+        return("It's a tie");
+    }
 }
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-
-console.log(playRound());
+console.log(playRound(playerSelection , computerSelection));
